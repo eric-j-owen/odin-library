cf863c7d20b979ebf60e576dc86dea55813fa9a6
eric-j-owen <145154417+eric-j-owen@users.noreply.github.com>
2024-02-09 12:02:05 -0500

    Adds read/unread bool property and button functionality

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const library = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, readBool) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.readBool = readBool;
 }
 
-function addBookToLibrary(title, author, pages) {
-    library.push(new Book(title, author, pages));
+function addBookToLibrary(title, author, pages, readBool) {
+    library.push(new Book(title, author, pages, readBool));
     displayBook(library);
 }
 
@@ -20,9 +21,13 @@ function displayBook(libraryArr) {
     const titleEl = document.createElement("p");
     const authorEl = document.createElement("p");
     const pagesEl = document.createElement("p");
+    const readEl = document.createElement("p");
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("delete");
-    deleteButton.textContent = "X";
+    deleteButton.textContent = "Delete Book";
+    const readButton = document.createElement("button");
+    readButton.classList.add("read");
+    readButton.textContent = "Read/Unread"
 
     bookEl.setAttribute("data-book-index", lastBookIndex);
 
@@ -31,23 +36,36 @@ function displayBook(libraryArr) {
         libraryArr[lastBookIndex].author
     );
     const pagesNode = document.createTextNode(libraryArr[lastBookIndex].pages);
+    const readNode = document.createTextNode(libraryArr[lastBookIndex].readBool);
 
     titleEl.appendChild(titleNode);
     authorEl.appendChild(authorNode);
     pagesEl.appendChild(pagesNode);
+    readEl.appendChild(readNode);
 
     bookEl.appendChild(titleEl);
     bookEl.appendChild(authorEl);
     bookEl.appendChild(pagesEl);
+    bookEl.appendChild(readEl);
     bookEl.appendChild(deleteButton);
+    bookEl.appendChild(readButton);
 
     librarySelector.appendChild(bookEl);
+
     deleteButton.addEventListener("click", (e) => {
         const bookElement = e.target.closest(".book");
         const bookIndex = bookElement.getAttribute("data-book-index");
         libraryArr.splice(bookIndex, 1);
         bookElement.remove();
     });
+
+    readButton.addEventListener("click", (e) => {
+        const bookElement = e.target.closest(".book");
+        const bookIndex = bookElement.getAttribute("data-book-index");
+        libraryArr[bookIndex].readBool = !libraryArr[bookIndex].readBool;
+        readEl.textContent = libraryArr[bookIndex].readBool ? "Read" : "Unread";
+    })
+    
 }
 
 const bookForm = document.querySelector(".add-book");
@@ -56,7 +74,11 @@ bookForm.addEventListener("submit", (e) => {
     const title = document.querySelector("#title").value;
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
-    addBookToLibrary(title, author, pages);
+    const radioButtons = document.querySelectorAll('input[name="readBool"]');
+    const selectedRadioButton = Array.from(radioButtons).find(button => button.checked);
+    const readBool = selectedRadioButton ? selectedRadioButton.value : null;
+
+    addBookToLibrary(title, author, pages, readBool);
 
     document.querySelector("#title").value = "";
     document.querySelector("#author").value = "";
